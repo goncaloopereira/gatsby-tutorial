713c8144b0901de29e5f0e4619cbc64c9105837b
Gonçalo Pereira <goncaloopereira@gmail.com>
2021-07-19 15:55:06 +0100

    second commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ const Index = () => {
     <Layout>
       <Metadata title="Home" description="This is my home page" />
       <h1>Home Page</h1>
-      <h2>I'm Gonçalo Pereira</h2>
+      <h2>I'm Gonçalo Pereira and I'm a Software Developer</h2>
     </Layout>
   )
 }
